57d3624038fe0643488a42549001cbe9f118abd4
mrpurwa <113221724+mrpurwa@users.noreply.github.com>
2022-09-14 19:13:20 +0700

    latihan2

---
+++ volume.py
@@ -0,0 +1,9 @@
+sisi = float(input("Masukkan sisi= "))
+volume_kubus = sisi * sisi * sisi
+print("volume kubus= ", volume_kubus)
+
+p = float(input("Masukkan p= "))
+l = float(input("Masukkan l= "))
+t = float(input("Masukkan t= "))
+volume_balok = p * l * t
+print("volume balok= ", volume_balok)
